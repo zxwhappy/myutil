fea23de195a81a5439d2a3c9c3012179bb3caee6
ZhangXiaoWei <zhangxiaoweihapply@qq.com>
2015-04-16 18:53:56 +0800

    增加array动态数组

---
+++ array.c
@@ -0,0 +1,118 @@
+#include "array.h"
+
+array_t *
+array_create(unsigned int n, size_t size)
+{
+    array_t *a;
+
+    a = malloc(sizeof(array_t));
+    if (a == NULL) {
+        return NULL;
+    }
+
+    if (array_init(a, n, size) != 0) {
+        return NULL;
+    }
+
+    return a;
+}
+
+
+void
+array_destroy(array_t **a)
+{
+    if (*a == NULL){
+        return;
+    }
+
+    if ((*a)->elts != NULL){
+        free((*a)->elts);
+        (*a)->elts = NULL;
+    }
+
+    free(*a);
+    (*a) = NULL;
+}
+
+
+void *
+array_push(array_t *a)
+{
+    void        *elt;
+    size_t       size;
+
+    if (a->nelts == a->nalloc) {
+
+        /* the array is full */
+
+        size = a->size * a->nalloc;
+
+        a->elts = realloc(a->elts, 2 * size);
+        if (a->elts == NULL) {
+            return NULL;
+        }
+
+        a->nalloc *= 2;
+    }
+
+    elt = (unsigned char *) a->elts + a->size * a->nelts;
+    a->nelts++;
+
+    return elt;
+}
+
+
+void *
+array_push_n(array_t *a, unsigned int n)
+{
+    void        *elt;
+    size_t       size;
+    unsigned int   nalloc;
+
+    size = n * a->size;
+
+    if (a->nelts + n > a->nalloc) {
+
+        /* the array is full */
+
+        nalloc = 2 * ((n >= a->nalloc) ? n : a->nalloc);
+
+        a->elts = realloc(a->elts, nalloc * a->size);
+        if (a->elts == NULL) {
+            return NULL;
+        }
+
+        a->nalloc = nalloc;
+    }
+
+    elt = (unsigned char *) a->elts + a->size * a->nelts;
+    a->nelts += n;
+
+    return elt;
+}
+/*
+typedef struct test{
+    int a;
+    int b;
+} test_t;
+
+void main(){
+    
+    array_t *arr = NULL;
+    arr = array_create(2,sizeof(test_t));
+    printf("creat:%d,%d,%d,%d,%d\n", arr->size, arr->nalloc, arr->nelts, 0, 0);
+    test_t *t1 = array_push(arr);
+    t1->a = 1;
+    t1->b = 2;
+    printf("creat:%d,%d,%d,%d,%d\n", arr->size, arr->nalloc, arr->nelts, t1->a, t1->b);
+    test_t *t2 = array_push(arr);
+    t2->a = 3;
+    t2->b = 4;
+    printf("creat:%d,%d,%d,%d,%d\n", arr->size, arr->nalloc, arr->nelts, t2->a, t2->b);
+    test_t *t3 = array_push(arr);
+    t3->a = 5;
+    t3->b = 6;
+    printf("creat:%d,%d,%d,%d,%d\n", arr->size, arr->nalloc, arr->nelts, t3->a, t3->b);
+    array_destroy(&arr);
+}
+*/
---
+++ array.h
@@ -0,0 +1,46 @@
+#ifndef _ARRAY_H_INCLUDED_
+#define _ARRAY_H_INCLUDED_
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _array_s {
+    void        *elts;
+    unsigned int   nelts;
+    size_t       size;
+    unsigned int   nalloc;
+} array_t;
+
+
+array_t *array_create(unsigned int n, size_t size);
+void array_destroy(array_t **a);
+void *array_push(array_t *a);
+void *array_push_n(array_t *a, unsigned int n);
+
+
+static inline int
+array_init(array_t *array, unsigned int n, size_t size)
+{
+    /*
+     * set "array->nelts" before "array->elts", otherwise MSVC thinks
+     * that "array->nelts" may be used without having been initialized
+     */
+
+    array->nelts = 0;
+    array->size = size;
+    array->nalloc = n;
+
+    if(array->elts != NULL) {
+        free(array->elts);
+        array->elts = NULL;
+    }
+
+    array->elts = malloc(n * size);
+    if (array->elts == NULL) {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+#endif /* _ARRAY_H_INCLUDED_ */
